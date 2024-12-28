fd8ecfcfead05fc3bf713621ccee384ea31b1102
Ryolin <manilrabia@gmail.com>
2024-12-28 15:09:01 +0100

    middlewares authentication + authorization

---
+++ api/const/ROLES.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class ROLES(Enum):
+    Patient = "Patient"
+
+    Medecin = "Medecin"
+    Infirmier = "Infirmier"
+    Laboratin = "Laboratin"
+    Radiologue = "Radiologue"
+    Personnel = "Personnel"
+
+    Admin = "Admin"
